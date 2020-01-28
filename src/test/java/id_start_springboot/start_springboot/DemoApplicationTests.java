84f2cf0ad33cf6c9ad72cc23336239c9c24ad2df
klack710 <willow710kut@gmail.com>
2020-01-28 16:50:22 +0900

    初期状態作成

---
+++ src/test/java/id_start_springboot/start_springboot/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package id_start_springboot.start_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
