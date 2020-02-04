e304208b28a1f79bc35271397d236067705147c2
klack710 <willow710kut@gmail.com>
2020-02-04 17:35:18 +0900

    リストやテーブルを表示させる練習

--- src/main/java/id_start_springboot/start_springboot/controller/IndexController.java
+++ src/main/java/id_start_springboot/start_springboot/controller/IndexController.java
@@ -1,13 +1,49 @@
 package id_start_springboot.start_springboot.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping("/")
-    public String index() {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Model model) {
+        String h1 = "Hello World";
+
+        List<String> items = new ArrayList<String>();
+        items.add("apple");
+        items.add("orange");
+        items.add("grape");
+
+        final int max_row = 5;
+        final int max_column = 10;
+
+        // tableを作成
+        List<List<String>> table = new ArrayList<List<String>>();
+        ArrayList<String> th_row = new ArrayList<String>();
+        ArrayList<String> row = new ArrayList<String>();
+        // thとなるヘッドの列作成
+        for (int i = 0; i < max_column; i++) {
+            th_row.add("th" + i);
+        }
+        // tdとなる列作成
+        for (int i = 0; i < max_row; i++) {
+            for (int j = 0; j < max_column; j++) {
+                row.add("{r:" + i + " c:" + j + "}");
+            }
+            table.add((ArrayList<String>) row.clone());
+            row.clear();
+        }
+
+        model.addAttribute("h1", h1);
+        model.addAttribute("items", items);
+        model.addAttribute("th_row", th_row);
+        model.addAttribute("table", table);
         return "index";
     }
 }
